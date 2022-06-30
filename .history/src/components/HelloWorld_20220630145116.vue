82b5da187115c76c522b639ae69cea1db7d89615
陈亚金 <adam.ch@foxmail.com>
2022-06-30 15:32:01 +0800

    init

---
+++ .history/src/components/HelloWorld_20220630145116.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <el-button type="button" @click="count++">count is: {{ count }}</el-button>
+</template>
+
+<style scoped lang="less">
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+  a {
+    color: #42b983;
+  }
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ .history/vite.config_20220630152038.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import DefineOptions from 'unplugin-vue-define-options/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+    host: '0.0.0.0'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    vue(),
+    DefineOptions(), 
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/, /\.vue\?vue/, // .vue
+      ],
+      dirs: ['./src'],
+      imports: ['vue'], // 自动导入vue的相关函数
+      dts: 'src/auto-import.d.ts', // 自动生成 'auto-import.d.ts'全局声明
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()], // ElementPlus按需加载：无需手动import，自动识别并注册
+    }),
+  ]
+})
